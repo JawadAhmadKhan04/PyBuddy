536011c2ee9d71490a21d5c1d5fd15cac6075c7a
JawadAhmadKhan04 <114512817+JawadAhmadKhan04@users.noreply.github.com>
2025-07-14 10:58:31 +0500

    Restructuring | Module 1 feedback implemented
    -> Entire codebase restructured, for modular approach
    -> hints getting displayed according to present file

---
+++ pybuddy/src/fileHelpers.js
@@ -0,0 +1,13 @@
+const vscode = require('vscode');
+
+async function openFolderInExplorer(folderPath) {
+	try {
+		const uri = vscode.Uri.file(folderPath);
+		await vscode.commands.executeCommand('vscode.openFolder', uri, false);
+		vscode.window.showInformationMessage("Opening folder");
+	} catch (err) {
+		vscode.window.showErrorMessage('Could not open folder: ' + err.message);
+	}
+}
+
+module.exports = { openFolderInExplorer };
